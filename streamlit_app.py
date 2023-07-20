ab1935e4897dfe5d45929a7d54cf6dcc188c15cb
code-to-joy <stefan.baeckstrand@gmail.com>
2023-07-20 20:45:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,17 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View our Fruit list - Add your favorites")
 
 def get_fruit_load_list(): 
   with my_cnx.cursor() as my_cur:
        my_cur.execute("select * from fruit_load_list")
        return my_cur.fetchall()
 
-if streamlit.button('Get fruit load list'):
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
