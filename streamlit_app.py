108c7d6916da1902735e3a4b298c0371f7193044
code-to-joy <stefan.baeckstrand@gmail.com>
2023-07-20 09:19:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title("My parents new healthy diner")
+
+streamlit.header("Menu")
+streamlit.text("Omega 3 and stuff")
+streamlit.text("Toast and cereal")
+streamlit.text("Buns")
+
